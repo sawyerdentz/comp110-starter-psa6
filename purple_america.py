d1d6a6e444d956ee83dd34a9d0c388fee1b85b4a
Sawyer Dentz <sdentz@sandiego.edu>
2024-11-13 14:16:00 -0800

    add all missing functions

--- purple_america.py
+++ purple_america.py
@@ -4,8 +4,8 @@ Module: purple_america
 Program for visualizing election results in interesting ways.
 
 Authors:
-1) Name - USD Email Address
-2) Name - USD Email Address
+1) Sawyer Dentz - sdentz@sandiego.edu
+2) Matt Oderlin - moderlin@sandiego.edu
 """
 
 import turtle
@@ -22,37 +22,153 @@ def draw_subregion(my_turtle, polygon_points):
     Returns:
     None
     """
-
-    pass  # replace this line with your implemenation
+    
+    my_turtle.penup()
+    my_turtle.goto(polygon_points[0])
+    my_turtle.pendown()
+    for coord in polygon_points[1:]:
+        my_turtle.goto(coord)
+    my_turtle.goto(polygon_points[0])
 
 
 def draw_filled_subregion(my_turtle, polygon_points, style, votes):
-    """ Replace this with a docstring comment with the correct format. """
+    """
+    Draws a ploygonal subregion filled in with the correct color
 
-    pass  # replace this line with your implemenation
+    Parameters:
+    my_turtle (type: Turtle) - The turtle that will do the drawing.
+    polygon_points (type: List) - List of tuples of the coordinates of the
+    polygonal region.
+    style (type: string) - string of the style the polygon should be filled with
+    votes (type: tuple) - tuple containing voting data
 
+    Returns:
+    None
+    """
+
+    if style == "black-white":
+        my_turtle.pencolor("black")
+        my_turtle.fillcolor("white")
+
+    elif style == "red-blue":
+        my_turtle.pencolor("white")
+        if votes[0] > votes[1] and votes[0] > votes[2]:
+            my_turtle.fillcolor("red")
+        elif votes[1] > votes[0] and votes[1] > votes[2]:
+            my_turtle.fillcolor("blue")
+        else:
+            my_turtle.fillcolor("gray")
+
+    elif style == "purple":
+        my_turtle.pencolor("white")
+        total_votes = votes[0] + votes[1] + votes[2]
+        if total_votes == 0:
+            my_turtle.fillcolor("gray")
+        else:
+            my_turtle.fillcolor((votes[0] / total_votes, votes[2] / total_votes, votes[1] / total_votes))
+
+    my_turtle.begin_fill()
+    draw_subregion(my_turtle, polygon_points)
+    my_turtle.end_fill()
 
 def read_subregion(geo_file):
-    """ Replace this with a docstring comment with the correct format. """
+    """
+    reads file and returns name of subregion and list of coordinates
 
-    return None, None  # replace this line with your implemenation
+    Parameters:
+    geo_file (type: file object) - the file object to be read
+
+    Return:
+    (type: tuple) - tuple containing name of subregion and list of coordinates
+    """
+
+    coord_list = []
+    subregion = geo_file.readline().rstrip()
+    while subregion == "":
+        subregion = geo_file.readline().rstrip()
+    geo_file.readline()
+    geo_file.readline()
+
+    line = geo_file.readline().rstrip()
+    while line != "":
+        fields = line.split()
+        coord_list.append((float(fields[0]), float(fields[1])))
+        line = geo_file.readline().rstrip()
+
+
+    return subregion, coord_list
 
 
 def draw_map(geo_filename, election_results, style):
-    """ Replace this with a docstring comment with the correct format. """
+    """
+    draws election map using map data, election results, and a specified style
 
-    pass  # replace this line with your implemenation
+    Parameters:
+    geo_filename (type: string) - the name of the file containg coordinates to be used to draw the map
+    election_results (type: string) - the name of the file containing election data
+    style (type: string) - the style the user would like the map to be drawn in
+
+    Return:
+    None
+    """
+
+    f = open(geo_filename, "r")
+    line = f.readline().strip().split()
+    min = (float(line[0]), float(line[1]))
+    line = f.readline().strip().split()
+    max = (float(line[0]), float(line[1]))
+    subregions = int(f.readline().strip())
+
+    turt = turtle.Turtle()
+
+    s = turtle.Screen()
+    s.setworldcoordinates(min[0], min[1], max[0], max[1])
+
+    turt.speed("fastest")
+    s.tracer(0, 0)
+
+    for i in range(subregions):
+        subregion, coords = read_subregion(f)
+        draw_filled_subregion(turt, coords, style, election_results.get(subregion, (0,0,0)))
+
+    f.close()
+    s.update()
+    s.exitonclick()
 
 
 def get_election_results(election_filename):
-    """ Replace this with a docstring comment with the correct format. """
+    """ 
+    Reads election results from a file and returns a dictionary containing data
 
-    return None  # replace this line with your implemenation
+    Parameter:
+    election_filename (type: string) - name of the file containing election results
+
+    Return:
+    (type: dictionary) - dictionary containing election data
+    """
+
+    dict = {}
+
+    f = open(election_filename, "r")
+
+    f.readline().split(",")
+   
+
+    for i in f:
+        data = i.split(",")
+        votes = (int(data[1]), int(data[2]), int(data[3]))
+
+        dict[data[0]] = votes
+
+    return dict  
 
 
 def main():
     """
-    Put your docstring comment here.
+    Asks user for input and runs functions to draw map acccordingly
+
+    Returns:
+    None
     """
 
     geo_filename = input("Enter the name of the geography file: ")
@@ -78,7 +194,8 @@ def main():
             print("Invalid selection!")
 
 
-    pass # Replace this line with your implementation of main
+    results = get_election_results(election_filename)
+    draw_map(geo_filename, results, style)
 
 
 """
